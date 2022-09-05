a9600a69ffa6dbedee0f455a3cb89c353487a8e3
zhl <741239739@qq.com>
2022-09-05 11:06:42 +0800

    create project

---
+++ src/main/java/com/web0905/Web0905Application.java
@@ -0,0 +1,13 @@
+package com.web0905;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Web0905Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Web0905Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/web0905/Web0905ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web0905;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Web0905ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
